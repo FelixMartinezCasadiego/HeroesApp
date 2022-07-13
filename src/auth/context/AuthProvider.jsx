a7f2e158954a878158159a7795b98071e6ecbe6e
FelixMartinezCasadiego <felix.martinezcasadiego@gmail.com>
2022-07-13 18:00:11 -0500

    auth

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -34,10 +34,19 @@ export const AuthProvider = ({children}) => {
         dispatch(action)
     }
 
+    const logout = () => {
+      localStorage.removeItem('user');
+      const action = {
+        type: types.logout
+      };
+      dispatch(action)
+    }
+
   return (
     <AuthContext.Provider value={{
       ...authState,
       login: login,
+      logout: logout,
     }} >
         {children}
     </AuthContext.Provider>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,8 @@ import { Routes, Route} from "react-router-dom";
 /* importaciones de pages */
 import { LoginPages } from '../auth/pages/LoginPages';
 import { HeroesRoutes } from '../heroes/routes/HeroesRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 
 export const AppRouter = () => {
@@ -11,9 +13,19 @@ export const AppRouter = () => {
 
         <Routes>
 
-            <Route path='login' element={< LoginPages /> } />
+            <Route path='/login' element={
+              <PublicRoute>
+                <LoginPages />
+              </PublicRoute>
+            } />
+            {/* <Route path='login' element={< LoginPages /> } /> */}
 
-            <Route path='/*' element={<HeroesRoutes />} />
+            <Route path='/*' element={
+              <PrivateRoute>
+                <HeroesRoutes />
+              </PrivateRoute>
+            } />
+            {/* <Route path='/*' element={<HeroesRoutes />} /> */}
 
       </Routes>
     </>
